99086c7b4775aa7a05752f2997d44df08c43ba77
Rickliba <henriquelibarino@gmail.com>
2022-06-05 18:16:07 -0300

    qualquer joça

--- script.js
+++ script.js
@@ -6,94 +6,83 @@ const pergunta = document.getElementById('questao')
 
 const questions = [
     {
-      question: 'Qual o maior piloto de todos os tempos',
-      answers: [
-        { text: 'Ayrton Senna', correct: true },
-        { text: 'Alain Prost', correct: false },
-        { text: 'Lewis Hamilton', correct: false },
-        { text: 'Michael Schumacher', correct: false }
-      ]
+        question: 'Qual o maior piloto de todos os tempos',
+        text1: 'Ayrton Senna',
+        text2: 'Alain Prost',
+        text3: 'Lewis Hamilton',
+        text4: 'Michael Schumacher',
     },
     {
-      question: 'Qual dessas curvas não faz parte do GP Brasil',
-      answers: [
-        { text: 'Curva do Balão', correct: true },
-        { text: 'Curva da Balança', correct: false },
-        { text: 'Laranjinha', correct: false },
-        { text: 'S do Senna', correct: false }
-      ]
+        question: 'Qual dessas curvas não faz parte do GP Brasil',
+        text1: 'Curva do Balão',
+        text2: 'Curva da Balança',
+        text3: 'Laranjinha',
+        text4: 'S do Senna',
+    },
+    
+    {
+        question: 'Qual o maior circuito já utilizado pela F1?',
+        text1: 'Pescara',
+        text2: 'Spa Francorchamps',
+        text3: 'Nurburgring Nordshcleife',
+        text4: 'SEI LÁ',
+      
     },
     {
-      question: 'Qual o maior circuito já utilizado pela F1?',
-      answers: [
-        { text: 'Pescara', correct: false },
-        { text: 'Spa Francorchamps', correct: true },
-        { text: 'Nurburgring Nordshcleife', correct: false },
-        { text: 'SEI LÁ', correct: false }
-      ]
-    },
-    {
-        question: 'Quem venceu o GP de São Paulo em 2021?',
-        answers: [
-          { text: 'Lewis Hamilton', correct: true },
-          { text: 'Max Verstappen', correct: false },
-          { text: 'Lando Norris', correct: false },
-          { text: 'Daniel Ricciardo', correct: false }
-        ]
+        question: 'Quem venceu o GP de São Paulo em 2021?',  
+        text1: 'Lewis Hamilton',
+        text2: 'Max Verstappen',
+        text3: 'Lando Norris',
+        text4: 'Daniel Ricciardo',
+        
       },
       {
         question: 'Qual piloto foi o campeão mundial em 2021?',
-        answers: [
-          { text: 'Sergio Perez', correct: false },
-          { text: 'Charles LeClerc', correct: false },
-          { text: 'Max Verstappen', correct: true },
-          { text: 'Lewis Hamilton', correct: false }
-        ]
+        text1: 'Sergio Perez',
+        text2: 'Charles LeClerc',
+        text3: 'Max Verstappen',
+        text4: 'Lewis Hamilton',
+        
       },
       {
         question: 'Quantos títulos tem os maiores campeões da Formula 1?',
-        answers: [
-          { text: '7', correct: true },
-          { text: '8', correct: false },
-          { text: '6', correct: false },
-          { text: '9', correct: false }
-        ]
+        
+        text1: '7',
+        text2: '8',
+        text3: '6',
+        text4: '9',
       },
       {
         question: 'Em qual equipe Rubinho correu ao lado de Michael Schumacher?',
-        answers: [
-          { text: 'Mclaren', correct: false },
-          { text: 'Williams', correct: false },
-          { text: 'Mercedes', correct: false },
-          { text: 'Ferrari', correct: true }
-        ]
+        text1: 'Mclaren',
+        text2: 'Williams',
+        text3: 'Mercedes',
+        text4: 'Ferrari',
+        
       },
       {
         question: 'Com qual equipe Sebastian Vettel conquistou seu tricampeonato mundial?',
-        answers: [
-          { text: 'RedBull', correct: true },
-          { text: 'Ferrari', correct: false },
-          { text: 'Aston Martin', correct: false },
-          { text: 'Toro Rosso', correct: false }
-        ]
+        text1: 'RedBull',
+        text2: 'Ferrari',
+        text3: 'Aston Martin',
+        text4: 'Toro Rosso',
+        
       },
       {
         question: 'Quantos carros tem no Grid?',
-        answers: [
-          { text: '22', correct: false },
-          { text: '18', correct: false },
-          { text: '20', correct: true },
-          { text: '19', correct: false }
-        ]
+        text1: '22',
+        text2: '18',
+        text3: '20',
+        text4: '19',
+        
       },
       {
         question: 'Quantos brasileiros já correram na Formula 1?',
-        answers: [
-          { text: '31', correct: true },
-          { text: '12', correct: false },
-          { text: '18', correct: false },
-          { text: '24', correct: false }
-        ]
+        text1: '31', 
+        text2: '12',
+        text3: '18',
+        text4: '24',  
+        
       }
   ]
 
