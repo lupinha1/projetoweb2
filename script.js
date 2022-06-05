635a1bc182a98d52ab3f9712ae147cc9a44d564f
Luis Paulo <lupinha2014@gmail.com>
2022-06-05 14:47:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,115 @@
+let question1 = {
+    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
+    r1: 'Laranjinha',
+    r2: 'S do Senna',
+    r3: 'Balança',
+    r4: 'Curva do museu',
+    rcorreta: 4
+}
+
+let question2 = {
+    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
+    r1: 'Laranjinha',
+    r2: 'S do Senna',
+    r3: 'Balança',
+    r4: 'Curva do museu',
+    rcorreta: 1
+}
+
+
+let question3 = {
+    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
+    r1: 'Laranjinha',
+    r2: 'S do Senna',
+    r3: 'Balança',
+    r4: 'Curva do museu',
+    rcorreta: 1
+}
+
+
+let question4 = {
+    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
+    r1: 'Laranjinha',
+    r2: 'S do Senna',
+    r3: 'Balança',
+    r4: 'Curva do museu',
+    rcorreta: 1
+}
+
+
+let question5 = {
+    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
+    r1: 'Laranjinha',
+    r2: 'S do Senna',
+    r3: 'Balança',
+    r4: 'Curva do museu',
+    rcorreta: 1
+}
+
+
+let question6 = {
+    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
+    r1: 'Laranjinha',
+    r2: 'S do Senna',
+    r3: 'Balança',
+    r4: 'Curva do museu',
+    rcorreta: 1
+}
+
+
+let question7 = {
+    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
+    r1: 'Laranjinha',
+    r2: 'S do Senna',
+    r3: 'Balança',
+    r4: 'Curva do museu',
+    rcorreta: 1
+}
+
+
+let question8 = {
+    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
+    r1: 'Laranjinha',
+    r2: 'S do Senna',
+    r3: 'Balança',
+    r4: 'Curva do museu',
+    rcorreta: 1
+}
+
+
+let question9 = {
+    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
+    r1: 'Laranjinha',
+    r2: 'S do Senna',
+    r3: 'Balança',
+    r4: 'Curva do museu',
+    rcorreta: 1
+}
+
+
+let question10 = {
+    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
+    r1: 'Laranjinha',
+    r2: 'S do Senna',
+    r3: 'Balança',
+    r4: 'Curva do museu',
+    rcorreta: 1
+}
+
+
+
+
+
+
+
+// function validar(){
+    
+// }
+
+// function irPraProx(){
+
+// }
+
+function clicarResposta(){
+
+}
