bda975995757171499cf5bef867b428b8db86690
Luis Paulo <lupinha2014@gmail.com>
2022-06-05 15:29:36 -0300

    Commit luis

--- script.js
+++ script.js
@@ -1,115 +1,44 @@
-let question1 = {
-    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
-    r1: 'Laranjinha',
-    r2: 'S do Senna',
-    r3: 'Balança',
-    r4: 'Curva do museu',
-    rcorreta: 4
-}
-
-let question2 = {
-    q: 'Quem foi o maior piloto de todos os tempos?',
-    r1: 'Ayrton Senna',
-    r2: 'Lewis Hamilton',
-    r3: 'Alain Prost',
-    r4: 'Michael Schumacher',
-    rcorreta: 1
-}
+const quizContainer = document.getElementById('quiz');
+const startBtn = document.getElementById('start-btn');
 
 
-let question3 = {
-    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
-    r1: 'Laranjinha',
-    r2: 'S do Senna',
-    r3: 'Balança',
-    r4: 'Curva do museu',
-    rcorreta: 1
-}
+const questions = [
+    {
+      question: 'What is 2 + 2?',
+      answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false }
+      ]
+    },
+    {
+      question: 'Who is the best YouTuber?',
+      answers: [
+        { text: 'Web Dev Simplified', correct: true },
+        { text: 'Traversy Media', correct: true },
+        { text: 'Dev Ed', correct: true },
+        { text: 'Fun Fun Function', correct: true }
+      ]
+    },
+    {
+      question: 'Is web development fun?',
+      answers: [
+        { text: 'Kinda', correct: false },
+        { text: 'YES!!!', correct: true },
+        { text: 'Um no', correct: false },
+        { text: 'IDK', correct: false }
+      ]
+    },
+    {
+      question: 'What is 4 * 2?',
+      answers: [
+        { text: '6', correct: false },
+        { text: '8', correct: true }
+      ]
+    }
+  ]
 
 
-let question4 = {
-    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
-    r1: 'Laranjinha',
-    r2: 'S do Senna',
-    r3: 'Balança',
-    r4: 'Curva do museu',
-    rcorreta: 1
-}
-
-
-let question5 = {
-    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
-    r1: 'Laranjinha',
-    r2: 'S do Senna',
-    r3: 'Balança',
-    r4: 'Curva do museu',
-    rcorreta: 1
-}
-
-
-let question6 = {
-    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
-    r1: 'Laranjinha',
-    r2: 'S do Senna',
-    r3: 'Balança',
-    r4: 'Curva do museu',
-    rcorreta: 1
-}
-
-
-let question7 = {
-    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
-    r1: 'Laranjinha',
-    r2: 'S do Senna',
-    r3: 'Balança',
-    r4: 'Curva do museu',
-    rcorreta: 1
-}
-
-
-let question8 = {
-    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
-    r1: 'Laranjinha',
-    r2: 'S do Senna',
-    r3: 'Balança',
-    r4: 'Curva do museu',
-    rcorreta: 1
-}
-
-
-let question9 = {
-    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
-    r1: 'Laranjinha',
-    r2: 'S do Senna',
-    r3: 'Balança',
-    r4: 'Curva do museu',
-    rcorreta: 1
-}
-
-
-let question10 = {
-    q: 'Qual dessas curvas não faz parte do GP do Brasil?',
-    r1: 'Laranjinha',
-    r2: 'S do Senna',
-    r3: 'Balança',
-    r4: 'Curva do museu',
-    rcorreta: 1
-}
-
-
-
-
-
-
-
-// function validar(){
-    
-// }
-
-// function irPraProx(){
-
-// }
-
-function clicarResposta(){
-
+function startQuiz(){
+    startBtn.classList.add('hide')
+    quizContainer.classList.remove('hide')
 }
